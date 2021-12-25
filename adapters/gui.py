d62868cd922c5ca27f99fd928f006d014da40028
Harry Cummings <harry@xlr3.com>
2021-12-25 12:38:07 +0000

    Tweak GUI display pixel spacing to match physical component

--- adapters/gui.py
+++ adapters/gui.py
@@ -4,7 +4,8 @@ from adafruit_midi.program_change import ProgramChange
 
 width = 16
 height = 10
-scale = 20
+scale_x = 15
+scale_y = 10
 
 def rgb_color(rgb):
     return "#%02x%02x%02x" % rgb  
@@ -13,7 +14,7 @@ class Application:
     def __init__(self):
         self.app = tk.Tk()
 
-        self.display = tk.Canvas(self.app, width=scale*width, height=scale*height, background="black")
+        self.display = tk.Canvas(self.app, width=scale_x*(width+2), height=scale_y*(height+2), background="black")
         self.display.grid(column = 0, columnspan=2, row=0)
 
         self.midi_in = tk.Entry(self.app)
@@ -34,7 +35,7 @@ class Application:
         self.display.delete("all")
         for y, row in enumerate(pixels):
             for x, pixel in enumerate(row):
-                self.display.create_rectangle(scale * x, scale * y, scale * (1+x) - 1, scale * (1+y) - 1, fill=rgb_color(pixel))
+                self.display.create_rectangle(2 + scale_x * (1+x), scale_y * (1+y), scale_x * (2+x) - 4, scale_y * (2+y) - 2, fill=rgb_color(pixel))
 
     def send_midi_in(self):
         if (self.midi_observer):
