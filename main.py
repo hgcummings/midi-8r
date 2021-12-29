6ebcce3d34e7a9a6841f88d16822c7d4c6770eb2
Harry Cummings <harry@xlr3.com>
2021-12-29 16:05:47 +0000

    Use file storage on hardware

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cores.diag import Diagnostic
 from ports.rgb_matrix import RgbMatrix
 from ports.shell_midi import ShellMidi
 from ports.encoder import Encoder
-from adapters.storage.transient import TransientStorage
+from adapters.storage.file import FileStorage
 from adapters.display import Display
 from adapters.noop import *
 from config import *
@@ -12,6 +12,6 @@ midi = ShellMidi()
 core = Diagnostic(
     midi,
     Encoder(ENCODER_PIN_BTN, ENCODER_PIN_CLK, ENCODER_PIN_DT, OUTPUT_PATCH_RANGE - 1),
-    TransientStorage(),
+    FileStorage('/storage.bin'),
     Display(RgbMatrix(RGB_MATRIX_PIN_NO))
 )
