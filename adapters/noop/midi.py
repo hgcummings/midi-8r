4fda459af244b7b546db6d0d10766729991f34b7
Harry Cummings <harry@xlr3.net>
2021-12-29 19:04:35 +0000

    Clean up unusued NoOp adapters

--- adapters/noop/midi.py
+++
@@ -1,6 +0,0 @@
-class NoOpMidi:
-    def observe_messages(self, _):
-        return
-
-    def send_message(self, _):
-        return
--- adapters/noop/storage.py
+++
@@ -1,6 +0,0 @@
-class NoOpStorage:
-    def get_preset(self, _):
-        return 0
-
-    def set_preset(self, _, __):
-        return
