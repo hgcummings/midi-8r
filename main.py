23136dcad0419b42b1e0ed28009f96e933eedd99
Harry Cummings <harry@xlr3.com>
2021-12-30 09:35:13 +0000

    Simplify display controller (buffer actual values to be sent to matrix)

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from cores.diag import Diagnostic
 from ports.rgb_matrix import RgbMatrix
-from ports.external_midi import ExternalMidi
+from ports.shell_midi import ShellMidi
 from ports.encoder import Encoder
 from adapters.storage.file import FileStorage
 from adapters.display import Display
-from adapters.noop import *
 from config import *
 
+midi = ShellMidi()
+matrix = RgbMatrix(RGB_MATRIX_PIN_DT, RGB_MATRIX_ROWS, RGB_MATRIX_COLS)
+
 core = Diagnostic(
-    ExternalMidi(MIDI_UART_PIN_TX, MIDI_UART_PIN_RX, MIDI_CHANNEL_OUT, MIDI_CHANNEL_IN),
+    midi,
     Encoder(ENCODER_PIN_BTN, ENCODER_PIN_CLK, ENCODER_PIN_DT, OUTPUT_PATCH_RANGE - 1),
     FileStorage('/storage.bin'),
-    Display(RgbMatrix(RGB_MATRIX_PIN_NO))
+    Display(matrix)
 )
--- ports/rgb_matrix.py
+++ ports/rgb_matrix.py
@@ -23,23 +23,33 @@ def ws2812():
     wrap()
 
 class RgbMatrix:
-    def __init__(self,pin):
+    def __init__(self, pin, rows, cols):
         self.sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(pin))
         self.sm.active(1)
-        self.buffer = [(0,0,0) for _ in range(NUM_ROWS * NUM_COLS)]
+        self.buffer = [0 for _ in range(rows * cols)]
         self.show_buffer()
+        self.rows = rows
+        self.cols = cols
 
     def show_pixels(self, pixels):
         for i in range(NUM_ROWS):
             for j in range(NUM_COLS):
                 if i < len(pixels) and j < len(pixels[i]):
-                    self.buffer[(i * NUM_COLS) + j] = (pixels[i][j][0] // DIM_FACTOR,
-                                                       pixels[i][j][1] // DIM_FACTOR,
-                                                       pixels[i][j][2] // DIM_FACTOR)
+                    self.buffer[(i * NUM_COLS) + j] = (
+                        ((pixels[i][j][0] // DIM_FACTOR) << 8) |
+                        ((pixels[i][j][1] // DIM_FACTOR) << 16) |
+                        (pixels[i][j][2] // DIM_FACTOR)
+                    )
                 else:
-                    self.buffer[(i * NUM_COLS) + j] = (0,0,0)
+                    self.buffer[(i * NUM_COLS) + j] = 0
+        self.show_buffer()
+
+    def clear(self):
+        for i in range(self.rows * self.cols):
+            self.buffer[i] = 0
         self.show_buffer()
 
     def show_buffer(self):
         for pixel in self.buffer:
-            self.sm.put((pixel[0] << 8) | (pixel[1] << 16) | pixel[2], 8)
+            self.sm.put(pixel, 8)
+
