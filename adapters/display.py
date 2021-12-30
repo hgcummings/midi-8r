38be2c9a373e5b36a2ed7385298e70223f99075a
Harry Cummings <harry@xlr3.com>
2021-12-30 10:54:30 +0000

    Tidy up display adapter (remove old-interface ConsoleDisplay)

--- adapters/display.py
+++ adapters/display.py
@@ -8,20 +8,14 @@ class Display:
         self.pixel_display.clear_buffer()
         self.pixel_display.show_buffer()
 
-    def show_patches(self, patch_in, patch_out, saved):
-        self.pixel_display.clear_buffer()
-        render_line(self.pixel_display, 0, 0, "▶" + str(patch_in), (255,255,255))
-        render_line(self.pixel_display, 0, 5, "◀" + str(patch_out),
-                                        (32, 255, 32) if saved else (127, 0, 0))
-        self.pixel_display.show_buffer()
-
     def show_text(self, text):
         self.pixel_display.clear_buffer()
         render_line(self.pixel_display, 0, 2, text, (255, 255, 255))
         self.pixel_display.show_buffer()
 
-class ConsoleDisplay:
-    def show_pixels(self, pixels):
-        for row in pixels:
-            line = [" " if c == (0,0,0) else "=" for c in row]
-            print("".join(line))
+    def show_patches(self, patch_in, patch_out, saved):
+        self.pixel_display.clear_buffer()
+        render_line(self.pixel_display, 0, 0, "▶{}".format(patch_in), (255,255,255))
+        render_line(self.pixel_display, 0, 5, "◀{}".format(patch_out),
+                                        (32, 255, 32) if saved else (127, 0, 0))
+        self.pixel_display.show_buffer()
