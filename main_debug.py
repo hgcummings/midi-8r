4b236ed764acf157f820cef849b4c218dfb71861
Harry Cummings <harry@xlr3.com>
2021-12-30 10:33:02 +0000

    Revert main script to external MIDI. Add convenience script for debugging.

--- main_debug.py
+++ main_debug.py
@@ -1,16 +1,17 @@
 from cores.diag import Diagnostic
-from ports.external_midi import ExternalMidi
+from ports.shell_midi import ShellMidi
 from ports.rgb_matrix import RgbMatrix
 from ports.encoder import Encoder
 from adapters.storage.file import FileStorage
 from adapters.display import Display
 from config import *
 
-matrix = RgbMatrix(RGB_MATRIX_PIN_DT, RGB_MATRIX_ROWS, RGB_MATRIX_COLS)
+midi = ShellMidi()
+display = Display(RgbMatrix(RGB_MATRIX_PIN_DT, RGB_MATRIX_ROWS, RGB_MATRIX_COLS))
 
 core = Diagnostic(
-    ExternalMidi(),
+    midi,
     Encoder(ENCODER_PIN_BTN, ENCODER_PIN_CLK, ENCODER_PIN_DT, OUTPUT_PATCH_RANGE - 1),
-    FileStorage('/storage.bin'),
-    Display(matrix)
+    FileStorage('/storage_debug.bin'),
+    display
 )
