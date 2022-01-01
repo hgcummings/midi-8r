56852c4906aae752cbf973450c670a6406c9a644
Harry Cummings <harry@xlr3.com>
2022-01-01 17:07:43 +0000

    Move source code into src subfolder

--- src/desktop.py
+++ src/desktop.py
@@ -2,12 +2,13 @@ from cores.patch import PatchCore
 from ports.gui import Application
 from adapters.storage.file import FileStorage
 from adapters.display import Display
-import os
+
+from pathlib import Path
 from config import *
 
 app = Application(RGB_MATRIX_ROWS, RGB_MATRIX_COLS)
 
-storage_root = os.path.dirname(__file__)
+storage_root = Path(__file__).resolve().parent.parent
 core = PatchCore(storage_root, app, app, Display(app))
 
 app.show_ui()
