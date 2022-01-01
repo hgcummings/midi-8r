1f5167fc02312ab6c87971fbe22a59eba501a502
Harry Cummings <harry@xlr3.com>
2022-01-01 18:02:20 +0000

    Add tests for component interface

---
+++ tests/cores/patch/components/test_interface.py
@@ -0,0 +1,26 @@
+from inspect import isclass
+from pkgutil import iter_modules
+from pathlib import Path
+from importlib import import_module
+
+import inspect
+
+def has_method(type, method_name):
+    matching_members = [member for name, member in inspect.getmembers(type, inspect.isfunction) if name == method_name]
+    return len(matching_members) == 1
+
+def test_interface():
+    package_dir = Path(__file__.replace("tests", "src")).resolve().parent
+    for (_, module_name, _) in iter_modules([package_dir]):
+
+        # import the module and iterate through its attributes
+        module = import_module(f"src.cores.patch.components.{module_name}")
+        for attribute_name in dir(module):
+            attribute = getattr(module, attribute_name)
+
+            if isclass(attribute) and attribute.__module__.startswith("src.cores.patch.components"):
+                # Add the class to this package's variables
+                assert has_method(attribute, "edit")
+                assert has_method(attribute, "update_value")
+                assert has_method(attribute, "switch")
+                assert has_method(attribute, "next")
