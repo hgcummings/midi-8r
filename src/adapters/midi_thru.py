86a1b9842e1cc33034d899561f69138929ade143
Harry Cummings <harry@xlr3.net>
2022-01-02 21:12:03 +0000

    Don't attempt to pass through unrecognised MIDI events

--- src/adapters/midi_thru.py
+++ src/adapters/midi_thru.py
@@ -1,4 +1,5 @@
 from adafruit_midi import MIDIMessage
+from adafruit_midi.midi_message import MIDIUnknownEvent
 
 class MidiThru:
     def __init__(self, midi_port):
@@ -13,7 +14,10 @@ class MidiThru:
     def on_message(self, msg):
         if (self.midi_observer):
             self.midi_observer(msg)
-        self.midi_port.send_message(msg, msg.channel)
+        # adafruit_midi doesn't seem to have a way of forwarding unrecognised messages,
+        # so we can't easily offer true MIDI through, but just forward what we can.
+        if not (msg is MIDIUnknownEvent):
+            self.midi_port.send_message(msg, msg.channel)
 
     def send_message(self, msg, channel=0):
         self.midi_port.send_message(msg, channel)
